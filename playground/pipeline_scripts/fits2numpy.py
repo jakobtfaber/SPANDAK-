7ba3ff83d34ac9dc7fd222263ae51b407b2848a0
Jakob Faber <jakobtfaber@gmail.com>
2020-10-18 13:40:04 -0700

    Moving away from SPANDAK-

--- playground/pipeline_scripts/fits2numpy.py
+++ playground/pipeline_scripts/fits2numpy.py
@@ -10,17 +10,16 @@ import os
 directory = argsv[1]
 
 def fits2numpy():
-	for fits in os.listdir(directory):
-		if fits.endswith('.fits') or fits.endswith('.ar') or fits.endswith('.tsch'):
-			npar = str(fits.split('.')[0]) + '.npy'
-			with open(npar, 'wb') as npar_file:		
-				arch = psrchive.Archive_load(directory + '/' + fits)
-				arch.dedisperse()
-				arch.remove_baseline()
-				#arch.convert_state('Stokes')
-				data = arch.get_data()
-				np.save(npar_file, data[:, 0, :, :].mean(0))
-				print('Array Written...')
+        for fits in os.listdir(directory):
+                if fits.endswith('.fits') or fits.endswith('.ar') or fits.endswith('.tsch'):
+                        npar = str(fits.split('.')[0]) + '.npy'
+                        with open(npar, 'wb') as npar_file:
+                                arch = psrchive.Archive_load(directory + '/' + fits)
+                                arch.dedisperse()
+                                arch.remove_baseline()
+                                #arch.convert_state('Stokes')
+                                data = arch.get_data()
+                                np.save(npar_file, data[:, 0, :, :].mean(0))
+                                print('Array Written...')
 
 fits2numpy()
-
